738c9e34aad450c0b1d611dd7426adc3dbefcba7
Andre Pankratz <andre@webwarelab.com>
2011-02-04 20:29:06 +0100

    Removed additional user attributes.
    Attributes will be provided by a resource provider later on.

--- lib/vidibus/user/mongoid.rb
+++ lib/vidibus/user/mongoid.rb
@@ -4,9 +4,6 @@ module Vidibus
       extend ActiveSupport::Concern
       included do
         field :uuid
-        field :email
-        field :name
-        validates :email, :name, :presence => true
         validates :uuid, :uuid => true
       end
     end
